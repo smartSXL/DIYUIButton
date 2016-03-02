6492f41ada034d7626d7baad83a88bab0107d5cc
玲玲 <suxl@shicaidai.com>
2016-03-02 16:09:12 +0800

    自定义button继承自UIView

---
+++ DIYUIButton/DIYUIButton/DIYButton.h
@@ -0,0 +1,13 @@
+//
+//  DIYButton.h
+//  DIYUIButton
+//
+//  Created by 001号 on 16/3/2.
+//  Copyright © 2016年 001号. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DIYButton : UIView
+
+@end
---
+++ DIYUIButton/DIYUIButton/DIYButton.m
@@ -0,0 +1,21 @@
+//
+//  DIYButton.m
+//  DIYUIButton
+//
+//  Created by 001号 on 16/3/2.
+//  Copyright © 2016年 001号. All rights reserved.
+//
+
+#import "DIYButton.h"
+
+@implementation DIYButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
